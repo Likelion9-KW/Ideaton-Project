883d4ab8603968a2226903c3b66347313910fc0a
chs98412 <79582366+chs98412@users.noreply.github.com>
2021-07-04 01:05:24 +0900

    views
    views

--- FlowerRecommend/recommend/views.py
+++ FlowerRecommend/recommend/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def recom(request):
+    return render(request, "flower.html")
--- FlowerRecommend/register/views.py
+++ FlowerRecommend/register/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def join(request):
+    return render(request, "join.html")
+
+
+def login(request):
+    return render(request, "login.html")
